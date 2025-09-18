3bd881b55e74fe9d5b34bd28dc2bf7a4bbae6034
OkaVatti <okavatti@proton.me>
2025-09-18 21:41:51 +0000

    frontend in the... idfk part ??????????? kill me :3

---
+++ apps/frontend/eslint.config.mjs
@@ -0,0 +1,6 @@
+// @ts-check
+import withNuxt from './blueberry-ib-build/eslint.config.mjs'
+
+export default withNuxt(
+  // Your custom configs here
+)
--- apps/frontend/nuxt.config.ts
+++ apps/frontend/nuxt.config.ts
@@ -1,5 +1,51 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  builder: "vite",
+  buildDir: "blueberry-ib-build",
+  dev: true,
+
+  devServer: {
+    host: "0.0.0.0",
+    port: 9876,
+    https: {
+      key: "/etc/nginx/ssl/lilith.key",
+      cert: "/etc/nginx/ssl/blueberry.lan.crt",
+    },
+  },
+
+  dir: {
+    assets: "app/assets",
+    layouts: "app/layouts",
+    pages: "app/pages",
+    public: "public",
+  },
+
+  modules: [
+    "@nuxtjs/sitemap",
+    "@nuxtjs/robots",
+    "@nuxt/eslint",
+    "@nuxt/image",
+    "@nuxt/fonts",
+    "@nuxtjs/color-mode",
+    "nuxt-feather-icons",
+    "@pinia/nuxt",
+  ],
+
+  imports: {
+    dirs: ['stores']
+  },
+
+  // Vite-specific server config (covers vite dev server)
+  vite: {
+    plugins: [tailwindcss()],
+    server: {},
+  },
+
+  typescript: {
+    strict: true,
+  },
+});
